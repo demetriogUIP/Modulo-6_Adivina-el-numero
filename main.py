ab8e9256e4f591de0c23a6077ea2d969bc9b35fe
yohelamat <yohel.amat@uip.edu.pa>
2025-11-14 17:02:22 +0000

    creamos la lmitacion de intentos a un maximo de 5 con un ciclo for y tuvimos que anular la variable adivinado

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import random
 
-print("=== ADIVINA EL NÚMERO (Versión básica) ===")
+print("=== ADIVINA EL NÚMERO (Versión con limite de intentos y validaciones) ===")
 
-# Genera número secreto entre 1 y 20
+# Genera número secreto entre 1 y 20(dificultad basica)
 numero_secreto = random.randint(1, 20)
 
-# Variable para controlar si adivinaron
-adivinado = False
+#Se limita la cantidad de intentos que puede tenr el jugador 
+INTENTOS_MAXIMO = 5
 
-while not adivinado:
-    try:
-        numero = int(input("Ingresa un número entre 1 y 20: "))
-    except ValueError:
-        print("Entrada inválida. Por favor ingresa un número.")
-        continue
+# Elimino la variable adivinado porque usare un bucle for
+# Variable para controlar si adivinaron
+#adivinado = False
+
+for intento in range(1, INTENTOS_MAXIMO + 1):
+    print(f"\n--- Se encuentra en su Intento {intento} de {INTENTOS_MAXIMO} ---")
+
+    while True:
+        try:
+            numero = int(input("Ingresa un número entre 1 y 20: "))
+        except ValueError:
+            print("Entrada inválida. Por favor ingresa un número.")
+            continue
+
+        #agrego otra validacion mas que solo aplica para la dificultad basica
+        if numero < 1 or numero > 20:
+            print("El numero ingresado esta fuera de la dificultad seleccionada")
+            continue
+
+        break # Sale del bucle 'while True' al obtener una entrada válida y en rango
 
     if numero < numero_secreto:
         print("El número secreto es MAYOR.")
@@ -21,4 +35,9 @@ while not adivinado:
         print("El número secreto es MENOR.")
     else:
         print("¡Correcto! Adivinaste el número.")
-        adivinado = True
+        # adivinado = True <-- Esta variable ya no existe y no es necesaria
+        break # Rompe el bucle 'for' al adivinar
+
+# Este es el 'else' del bucle 'for', se ejecuta si el 'for' termina sin un 'break'
+else: 
+    print(f"\nSe acabaron los intentos. El número secreto era: {numero_secreto}")
